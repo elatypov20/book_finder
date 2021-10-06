50ff95036ac26ec1c9765fb7a77cc72d8a2e777c
Emil Latypov <e.latypov@omp.ru>
2021-10-06 22:44:18 +0300

    implement Database class constructor

--- src/LibraryDatabase.java
+++ src/LibraryDatabase.java
@@ -1,15 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.FileReader;
-
+import java.util.Scanner;
+import java.lang.StringBuilder;
 
 public class LibraryDatabase extends Database{
     private List<Book> books;
 
     private static LibraryDatabase instance;
 
-    private LibraryDatabase(FileReader file){
-        super(file);
+    private LibraryDatabase(String databaseName) throws FileNotFoundException{
+        super(databaseName);
 
         // TODO
         // implement parsing rows into Books
@@ -56,18 +59,52 @@ public class LibraryDatabase extends Database{
 
 class Database {
     protected List<List<String>> rows;
-    protected FileReader file;
+    protected final String databaseName;
 
-    protected Database(FileReader file){
-        file = file;
-        // TODO
-        // implement reading rows from file
+    protected Database(String fileName) throws FileNotFoundException {
+        this.databaseName = fileName;
+        Scanner scan = new Scanner(new FileReader(databaseName));
+        rows = new ArrayList<List<String>>();
+
+
+        while (scan.hasNextLine()){
+            String line = scan.nextLine();
+            if (line.equals("")) continue;
+
+            ArrayList<String> tokens = new ArrayList<>();
+
+            int pos = 0;
+
+            while (pos <= line.length() - 1) {
+                StringBuilder currentToken = new StringBuilder();
+
+                if (line.charAt(pos) == '"') {
+                    pos++;
+                    while (line.charAt(pos) != '"' || line.charAt(pos + 1) != ',') {
+                        currentToken.append(line.charAt(pos));
+                        pos++;
+                    }
+                    pos++;
+
+                } else {
+                    while (pos < line.length() && line.charAt(pos) != ',') {
+                        currentToken.append(line.charAt(pos));
+                        pos++;
+                    }
+                }
+                pos++;
+                tokens.add(currentToken.toString());
+            }
+            rows.add(tokens);
+        }
+
+//        for (List<String> row : rows)
+//            System.out.println(row);
+
+        scan.close();
     }
 
-    protected void updateRows(List<String> rows){
-        // TODO
-        // when some rows are changed
-        // imlement uploading changes to the disk
-        
+    protected void flush(){
+
     }
 }
